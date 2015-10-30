2b553c5cf00d6d5d525d4e9632e5e407725df11a
Chris Boyd <boyd111c@gmail.com>
2015-10-29 23:09:01 -0600

    Update ATMODE.ino
    Added credit to author

--- Arduino/ATMODE.ino
+++ Arduino/ATMODE.ino
@@ -1,3 +1,20 @@
+ /*
+   Software serial multple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+
+ This example code is in the public domain.
+*/
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(10, 11); // RX, TX
