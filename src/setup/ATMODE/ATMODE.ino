753c47dcdf1dc11f8fdc25b8512d4e015eda629c
iwanttoeatyo <boyd111c@gmail.com>
2015-12-01 00:22:16 -0600

    test files

--- src/setup/ATMODE/ATMODE.ino
+++ src/setup/ATMODE/ATMODE.ino
@@ -19,7 +19,8 @@
 
 SoftwareSerial mySerial(10, 11); // RX, TX
 
-void setup() {
+void setup() 
+{
 
 Serial.begin(9600);
 
@@ -28,19 +29,12 @@ pinMode(9,OUTPUT); digitalWrite(9,HIGH);
 Serial.println("Enter AT commands:");
 
 mySerial.begin(38400);
-
 }
 
 void loop()
-
 {
-
-if (mySerial.available())
-
-Serial.write(mySerial.read());
-
-if (Serial.available())
-
-mySerial.write(Serial.read());
-
+    if (mySerial.available())
+        Serial.write(mySerial.read());
+    if (Serial.available())
+        mySerial.write(Serial.read());
 }
